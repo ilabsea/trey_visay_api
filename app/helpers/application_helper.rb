d1660cf0c8095309929c33ffe6f06ec4df8bc730
Sokly <hengsokly23@gmail.com>
2018-01-04 15:01:09 +0700

    style sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-
   def css_id_name
+    prefix = params['controller'].downcase.split('/').join('-')
+    subfix = params['action']
+
+    return "#{prefix}-#{subfix}"
   end
 end
