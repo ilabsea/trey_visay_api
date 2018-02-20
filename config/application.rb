eb609ef93a92b09d739226f78715ce80191eeca0
Sokly <hengsokly23@gmail.com>
2018-02-20 17:01:32 +0700

    set environment variable

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module TreyVisayApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
   end
 end
