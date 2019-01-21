2d820441a186631c8dc7c63eb9784d4732326911
Sokly <hengsokly23@gmail.com>
2019-01-21 15:09:01 +0700

    fix login for counsellor

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,18 +2,10 @@
 
 class GamesController < ApplicationController
   before_action :authenticate_account!
-  before_action :authenticate_game_owner!
 
-  expose(:game) { Game.find(params[:id]) }
   def show
     @user = User.find(params[:user_id])
     @game = Game.find(params[:id])
     @schools = @game.goal_career.schools.includes(:departments, :department_majors, :majors)
-  end
-
-  private
-
-  def authenticate_game_owner!
-    head :forbidden unless (current_account.role == Account::ROLE[0]) || (game.user.school_name == current_account.schools[0])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_account!
-  before_action :authenticate_user_owner!
-  expose(:user) { User.find(params[:id]) }
 
   def index
     @users = User.filter(school_id: params[:school], grade: params[:grade]).includes(:games, :personal_understandings).page(page_params).per(20)
@@ -13,11 +11,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  end
-
-  private
-
-  def authenticate_user_owner!
-    head :forbidden unless (current_account.role == Account::ROLE[0]) || (user.school_name == current_account.schools[0])
   end
 end
