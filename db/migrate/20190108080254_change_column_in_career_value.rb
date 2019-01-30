eb55d36cefb324746d2010ef2277b1ac39150776
Sokly <hengsokly23@gmail.com>
2019-01-30 14:09:25 +0700

    change some careers from vocational to social

---
+++ db/migrate/20190108080254_change_column_in_career_value.rb
@@ -0,0 +1,9 @@
+class ChangeColumnInCareerValue < ActiveRecord::Migration[5.1]
+   def change
+  	category = Characteristic.find_by(title: "បុគ្គលិកលក្ខណៈបុគ្គល បែបសង្គម")
+    careerTitles = ['អ្នកថែទាំកុមារ', 'គ្រូបង្រៀនជនពិការ', 'អ្នកបណ្ដុះបណ្ដាលវិជ្ជាជីវៈ']
+
+    careers = Career.where(name: careerTitles)
+    careers.update_all(categorizable_type: 'Characteristic', categorizable_id: category.id)
+  end
+end
