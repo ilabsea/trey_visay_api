07eecb98bbb116b700c75980b05a2bdd78a7d729
Sokly <hengsokly23@gmail.com>
2019-02-22 17:00:30 +0700

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users do
   	resources :games
   end
+  resource :about, only: [:show]
   resources :accounts
   namespace :api do
     namespace :v1 do
--- db/migrate/20190108080254_change_column_in_career_value.rb
+++ db/migrate/20190108080254_change_column_in_career_value.rb
@@ -4,6 +4,8 @@ class ChangeColumnInCareerValue < ActiveRecord::Migration[5.1]
     careerTitles = ['អ្នកថែទាំកុមារ', 'គ្រូបង្រៀនជនពិការ', 'អ្នកបណ្ដុះបណ្ដាលវិជ្ជាជីវៈ']
 
     careers = Career.where(name: careerTitles)
+    return if careers.blank?
+
     careers.update_all(categorizable_type: 'Characteristic', categorizable_id: category.id)
   end
 end
