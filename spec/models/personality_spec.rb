c35891a3839df687c67f11c29022581115064b9d
Sokly <hengsokly23@gmail.com>
2019-03-13 13:50:42 +0700

    fix spec

--- spec/models/personality_spec.rb
+++ spec/models/personality_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Personality, type: :model do
   it { is_expected.to have_many(:personality_selections) }
   it { is_expected.to validate_presence_of(:name_km) }
-  it { is_expected.to validate_inclusion_of(:category).in_array(%w(Realistic Investigative Artistic Social Enterprising Conventional)) }
+  it { is_expected.to validate_inclusion_of(:category).in_array(%w(realistic investigative artistic social enterprising conventional)) }
 end
